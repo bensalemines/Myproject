8b652a1cab11f71e7c35180f2504584cb9d34134
bensalemines <inessbensalem910@gmail.com>
2022-08-28 19:30:39 +0100

    changing server

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import {BrowserRouter} from 'react-router-dom'
 import axios from 'axios'
 import {Provider} from 'react-redux'
 import store from './store'
-axios.defaults.baseURL='https://campingunderthestars.herokuapp.com/';
+axios.defaults.baseURL='https://campingunderthestars.herokuapp.com';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
